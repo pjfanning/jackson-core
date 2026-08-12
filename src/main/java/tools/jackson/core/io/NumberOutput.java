6c356a49760d42107476171b4e6de7617070c548
PJ Fanning <pjfanning@users.noreply.github.com>
2026-08-12 11:44:54 +0100

    Update NumberOutput.java

--- src/main/java/tools/jackson/core/io/NumberOutput.java
+++ src/main/java/tools/jackson/core/io/NumberOutput.java
@@ -1,7 +1,6 @@
 package tools.jackson.core.io;
 
-import tools.jackson.core.io.schubfach.DoubleToDecimal;
-import tools.jackson.core.io.schubfach.FloatToDecimal;
+import tools.jackson.core.io.xjb.XJBWriter;
 
 public final class NumberOutput
 {
@@ -262,11 +261,11 @@ public final class NumberOutput
 
     /**
      * @param v double
-     * @param useFastWriter whether to use Schubfach algorithm to write output (default false)
+     * @param useFastWriter whether to use XJB algorithm to write output (default false)
      * @return double as a string
      */
     public static String toString(final double v, final boolean useFastWriter) {
-        return useFastWriter ? DoubleToDecimal.toString(v) : Double.toString(v);
+        return useFastWriter ? XJBWriter.toString(v) : Double.toString(v);
     }
 
     /**
@@ -279,11 +278,11 @@ public final class NumberOutput
 
     /**
      * @param v float
-     * @param useFastWriter whether to use Schubfach algorithm to write output (default false)
+     * @param useFastWriter whether to use XJB algorithm to write output (default false)
      * @return float as a string
      */
     public static String toString(final float v, final boolean useFastWriter) {
-        return useFastWriter ? FloatToDecimal.toString(v) : Float.toString(v);
+        return useFastWriter ? XJBWriter.toString(v) : Float.toString(v);
     }
 
     /*
